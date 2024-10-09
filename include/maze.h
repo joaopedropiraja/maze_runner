cac3661431aa9a069f96340d63b79554473b371d
João Pedro Lima Pirajá <jplp100@hotmail.com>
2024-10-08 21:36:17 -0300

    Parte 2

--- include/maze.h
+++ include/maze.h
@@ -6,11 +6,9 @@
 
 typedef struct maze_s Maze;
 
-Maze *m_create(unsigned rowsNum, unsigned colsNum);
 void m_delete(Maze *m);
 
 Maze *m_loadFromFile(char *fileName);
-bool m_isValidLocation(Maze *m, int row, int col);
 bool m_hasSolution(Maze *m);
 
 void m_print(Maze *m);
--- src/maze.c
+++ src/maze.c
@@ -1,23 +1,39 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <pthread.h>
 
 #include "maze.h"
 #include "stack.h"
 #include "msgassert.h"
+
+// Private functions declarations
+static Maze *m_create(unsigned rowsNum, unsigned colsNum);
+static bool m_isValidLocation(Maze *m, int row, int col);
+static void *walk(void *data);
+typedef struct thread_data_s {
+    Maze *m;
+    Position p;
+} ThreadData;
+
+// Implementations
 
 struct maze_s {
     char **data;
     unsigned rowsNum;
     unsigned colsNum;
     Position initial;
+    bool foundExit;
+    unsigned activeThreadsNum;
 };
 
-Maze *m_create(unsigned rowsNum, unsigned colsNum) {
+static Maze *m_create(unsigned rowsNum, unsigned colsNum) {
     Maze *m = (Maze*)malloc(sizeof(Maze));
     msgassert(m != NULL, "Memory allocation failed for maze: { rowsNum: %u, colsNum: %u }\n", rowsNum, colsNum);
 
     m->rowsNum = rowsNum;
     m->colsNum = colsNum;
+    m->foundExit = false;
+    m->activeThreadsNum = 0;
 
     m->data = (char**)malloc(rowsNum * sizeof(char*));
     msgassert(m->data != NULL, "Memory allocation failed for maze: { rowsNum: %u, colsNum: %u }\n", rowsNum, colsNum);
@@ -63,10 +79,10 @@ Maze *m_loadFromFile(char *fileName) {
     return m;
 };
 
-bool m_isValidLocation(Maze *m, int row, int col) {
+static bool m_isValidLocation(Maze *m, int row, int col) {
     if (
-        row < 0 || row > (int)m->rowsNum || 
-        col < 0 || col > (int)m->colsNum
+        row < 0 || row >= (int)m->rowsNum || 
+        col < 0 || col >= (int)m->colsNum
     ) return false;
 
     return m->data[row][col] == 'x' || m->data[row][col] == 's';
@@ -83,40 +99,95 @@ void m_print(Maze *m) {
     printf("\n\n");    
 };
 
-bool m_hasSolution(Maze *m) {
-    Stack *s = st_create(2);
-    st_insert(s, m->initial);
+static void *walk(void *data) {
+  ThreadData *threadData = data;
+  if (threadData->m->data[threadData->p.row][threadData->p.col] == 's') {
+    threadData->m->foundExit = true;
+    threadData->m->activeThreadsNum--;
 
-    while (!st_empty(s)) {
-        Position sn = st_pop(s);
+    free(threadData);
+    return NULL;
+  }
 
-        if (m->data[sn.row][sn.col] == 's') return true;
+  sleep_ms(15);
+  threadData->m->data[threadData->p.row][threadData->p.col] = '.';
 
-        m->data[sn.row][sn.col] = '.';
-
-        m_print(m);
-        sleep_ms(50);
-
-        int row = sn.row + 1;
-        int col = sn.col;
-        if (m_isValidLocation(m, row, col)) 
-            st_insert(s, (Position){ row, col });
-
-        row = sn.row - 1;
-        if (m_isValidLocation(m, row, col))
-            st_insert(s, (Position){ row, col });
-
-        row = sn.row;
-        col = sn.col + 1;
-        if (m_isValidLocation(m, row, col)) 
-            st_insert(s, (Position){ row, col });        
-
-        col = sn.col - 1;
-        if (m_isValidLocation(m, row, col))
-            st_insert(s, (Position){ row, col });
-    }
-
-    st_delete(s);
+  int row = threadData->p.row + 1;
+  int col = threadData->p.col;
+  if (m_isValidLocation(threadData->m, row, col)) {
+    ThreadData *newThreadData1 = malloc(sizeof(ThreadData));
+    newThreadData1->m = threadData->m;
     
-    return false;
+    pthread_t newThread1;
+    newThreadData1->p = (Position){ row, col };
+
+    threadData->m->activeThreadsNum++;
+    pthread_create(&newThread1, NULL, walk, newThreadData1);
+    pthread_detach(newThread1);
+  }
+
+  row = threadData->p.row - 1;
+  if (m_isValidLocation(threadData->m, row, col)) {
+    ThreadData *newThreadData2 = malloc(sizeof(ThreadData));
+    newThreadData2->m = threadData->m;
+
+    pthread_t newThread2;
+    newThreadData2->p = (Position){ row, col };
+
+    threadData->m->activeThreadsNum++;
+    pthread_create(&newThread2, NULL, walk, newThreadData2);
+    pthread_detach(newThread2);    
+  }
+
+  row = threadData->p.row;
+  col = threadData->p.col + 1;
+  if (m_isValidLocation(threadData->m, row, col)) {
+    ThreadData *newThreadData3 = malloc(sizeof(ThreadData));
+    newThreadData3->m = threadData->m;
+
+    pthread_t newThread3;
+    newThreadData3->p = (Position){ row, col };
+
+    threadData->m->activeThreadsNum++;
+    pthread_create(&newThread3, NULL, walk, newThreadData3);
+    pthread_detach(newThread3);    
+  }     
+
+  col = threadData->p.col - 1;
+  if (m_isValidLocation(threadData->m, row, col)) {
+    ThreadData *newThreadData4 = malloc(sizeof(ThreadData));
+    newThreadData4->m = threadData->m;
+
+    pthread_t newThread4;
+    newThreadData4->p = (Position){ row, col };
+
+    threadData->m->activeThreadsNum++;
+    pthread_create(&newThread4, NULL, walk, newThreadData4);
+    pthread_detach(newThread4);    
+  }
+
+  threadData->m->activeThreadsNum--;
+
+  free(threadData);
+  return NULL;
+}
+
+bool m_hasSolution(Maze *m) {
+
+  pthread_t startThread;
+  ThreadData *threadData = malloc(sizeof(ThreadData));
+  
+  threadData->m = m;
+  threadData->p = m->initial;
+
+  m->activeThreadsNum++;
+  pthread_create(&startThread, NULL, walk, threadData);
+  pthread_detach(startThread);
+
+  while (!m->foundExit && m->activeThreadsNum > 0) {
+    m_print(m);
+    sleep_ms(50);
+  }
+
+  return m->foundExit;
 };
